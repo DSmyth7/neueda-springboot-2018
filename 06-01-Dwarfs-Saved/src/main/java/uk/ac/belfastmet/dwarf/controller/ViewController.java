89dfb7ad9d8f3c03cc2fc40ec8296cc7fc27cc29
DSmyth7 <d-jsmyth@hotmail.co.uk>
2018-12-05 16:18:26 +0000

    Added functionality to dwarf app to delete dwarfs.

--- 06-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarf/controller/ViewController.java
+++ 06-01-Dwarfs-Saved/src/main/java/uk/ac/belfastmet/dwarf/controller/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.belfastmet.dwarf.repository.DwarfRepository;
 
@@ -21,12 +21,29 @@ public class ViewController {
 		this.dwarfRepository = dwarfRepository;
 	}
 	
+	@GetMapping("/dwarfs")
+	public String viewAllDwarf(Model model) {
+		model.addAttribute("pageTitle", "Search!");
+		model.addAttribute("headingOne","");
+		model.addAttribute("dwarfs", this.dwarfRepository.findAll());
+		return "dwarf.html";
+	
+	}
+	
 	@GetMapping("/view/{dwarfId}")
 	public String viewDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model) {
 		model.addAttribute("pageTitle", "Search!");
 		model.addAttribute("headingOne","");
 		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
 		return "view.html";
+	
+	}
+	
+	@GetMapping("/delete/{dwarfId}")
+	public String deleteDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message", "Successfully deleted dwarf!");
+		this.dwarfRepository.deleteById(dwarfId);
+		return "redirect:/dwarfs";
 	
 	}
 

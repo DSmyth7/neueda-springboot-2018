296523ae7421beaf5190460a4d2769fa6492f151
DSmyth7 <d-jsmyth@hotmail.co.uk>
2018-12-11 13:31:02 +0000

    Added functionality to view all passengers on the same page. Can now delete passengers.

--- 07-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/titanicconsumer/controller/PassengerController.java
+++ 07-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/titanicconsumer/controller/PassengerController.java
@@ -2,13 +2,17 @@ package uk.ac.belfastmet.titanicconsumer.controller;
 
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import uk.ac.belfastmet.titanicconsumer.domain.AllPassengers;
 import uk.ac.belfastmet.titanicconsumer.domain.Passenger;
@@ -56,13 +60,48 @@ public class PassengerController {
 		return "passengersPage.html";
 	}
 	
-//	@GetMapping("/passengers")
-//	public String passenger(Model model) {
-//		model.addAttribute("pageTitle", "Titanic Passenger!");
-//		ArrayList<Passenger> passengers = this.passengerService.list();
-//		model.addAttribute("passengers", passengers);
-//		return "passengersPage.html";
-//	
+	@GetMapping("/passengers")
+	public String passenger(Model model) {
+		
+		model.addAttribute("pageTitle", "Titanic Passengers!");
+		
+		ArrayList<Passenger> passengers = this.passengerService.list();
+		model.addAttribute("passengers", passengers);
+		
+		return "passengersPage.html";
+	
+	}
+	
+//	@GetMapping("/add")
+//	public String addPassenger(Model model) {
+//		model.addAttribute("pageTitle", "Titanic Add!");
+//		model.addAttribute("passenger", new Passenger());
+//		return "edit.html";
 //	}
+//	
+//	@GetMapping("/edit/{passengerId}")
+//	public String editPassenger(@PathVariable("passengerId") Integer passengerId, Model model) {
+//		model.addAttribute("pageTitle", "Edit Passenger!");
+//		model.addAttribute("passenger", this.passengerService.update(passengerId));
+//		return "edit.html";
+//	}
+//
+//	@PostMapping("/save")
+//	public String savePassenger(@Valid Passenger passenger, BindingResult bindingResult, Model model) {
+//		if (bindingResult.hasErrors()) {
+//			return "edit.html";
+//		} else {
+//			Passenger savedPassenger = this.savePassenger(passenger, bindingResult, model)(passenger);
+//			return "redirect:/view/" + passenger.getPassengerId();
+//		}
+//	}
+	
+	@GetMapping("/delete/{passengerId}")
+	public String deletePassenger(@PathVariable("passengerId") Integer passengerId, Model model, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("message", "Successfully deleted passenger!");
+		this.passengerService.delete(passengerId);
+		return "redirect:/titanic/passengers";
+	
+	}
 	
 }
--- 07-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/titanicconsumer/service/PassengerServiceImpl.java
+++ 07-02-TitanicConsumer/src/main/java/uk/ac/belfastmet/titanicconsumer/service/PassengerServiceImpl.java
@@ -1,6 +1,7 @@
 package uk.ac.belfastmet.titanicconsumer.service;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,7 @@ public class PassengerServiceImpl implements PassengerService{
 	public ArrayList<Passenger> list(){
 		
 		String listPassengerUrl = this.apiUrl + "/passengers";
-		
+			
 		AllPassengers allPassengers = this.restTemplate.getForObject(listPassengerUrl, AllPassengers.class);
 		
 		return allPassengers.getAllPassengers();		
